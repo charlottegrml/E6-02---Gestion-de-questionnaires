a6987ea05e54426a8ee8cbdd67c6f655a4deadc0
charlottegrml <charlotte.gourmelond@gmail.com>
2025-05-23 12:23:20 +0200

    Add files via upload

---
+++ GestionQuestionnaires/GestionQuestionnaires/Utilitaires/UtilisateurConnecte.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionQuestionnaires.Utilitaires
+{
+    public static class UtilisateurConnecte
+    {
+        public static int Id { get; set; }
+        public static string NomUtilisateur { get; set; }
+        public static string Nom { get; set; }
+        public static string Prenom { get; set; }
+
+        public static void Deconnexion()
+        {
+            Id = 0;
+            NomUtilisateur = null;
+            Nom = null;
+            Prenom = null;
+        }
+    }
+
+}
